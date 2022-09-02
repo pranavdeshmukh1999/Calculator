c9c9079baad56077772b5cfeb44a15ec470eb474
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-02 17:18:41 +0530

    Added:Addition logic

--- src/comcalculator/Calcultor.java
+++ src/comcalculator/Calcultor.java
@@ -5,6 +5,14 @@ public class Calcultor {
 	public static void main(String[] args) {
     
 		System.out.println("We are doing calcultor program");
+		  
+		   int num1=20;
+		   int num2=30;
+		   
+		   int sum=num1+num2;
+		   
+		   System.out.println("sum is: "+sum);
+		    
 	}
 
 }
