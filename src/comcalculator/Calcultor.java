76beb5c6212ca9db5c2f27675e5ed3ee16a30ba5
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-02 18:14:04 +0530

    Added:substraction logic

--- src/comcalculator/Calcultor.java
+++ src/comcalculator/Calcultor.java
@@ -10,8 +10,11 @@ public class Calcultor {
 		   int num2=30;
 		   
 		   int sum=num1+num2;
+		   int sub=num1-num2;
 		   
 		   System.out.println("sum is: "+sum);
+		   System.out.println("sub is: "+sub);
+		    
 		    
 	}
 
